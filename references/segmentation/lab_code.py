2903c5cd702911c73a4240f2f2379535053e11f9
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-09-05 15:11:02 +0900

    Update lab_code.py

--- references/segmentation/lab_code.py
+++ references/segmentation/lab_code.py
@@ -1,79 +1,3 @@
-'''
-import torch
-from PIL import Image, ImageOps
-import matplotlib.pyplot as plt
-import torchvision.transforms as T
-import numpy as np
-import cv2
-import time
-import os
-
-# Define the helper function
-def decode_segmap(image, nc=21): 
-  label_colors = np.array([(0, 0, 0),  # 0=background
-               # 1=ceiling, 2=floor, 3=wall, 4=blockage, 5=glassdoor
-               (100, 100, 100), (255, 255, 255), (50, 50, 50), (0, 0, 0), (128, 0, 128),
-               # 6=table, 7=wall, 8=window, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-
-  r = np.zeros_like(image).astype(np.uint8)
-  g = np.zeros_like(image).astype(np.uint8)
-  b = np.zeros_like(image).astype(np.uint8) 
-
-  for l in range(0, nc):
-    idx = image == l
-    r[idx] = label_colors[l, 0]
-    g[idx] = label_colors[l, 1]
-    b[idx] = label_colors[l, 2]   
-
-  rgb = np.stack([r, g, b], axis=2)
-  return rgb
-
-
-# load pth model
-model = torch.load('D:\\pytorch_cuda\\segmentaion_data\\dataset3\\model\\model_2.pth')
-# set model to inference mode
-model.eval()
-#print(model)
-
-folder_path = 'D:\\pytorch_cuda\\segmentaion_data\\dataset3\\test'
-for filename in os.listdir(folder_path):  # 폴더 내의 모든 파일에 대해 반복
-  if filename.endswith(".jpg") or filename.endswith(".jfif") or filename.endswith(".webp"):  # 지원하는 파일 형식 검사
-    image_path = os.path.join(folder_path, filename)  # 파일 경로 생성
-    img = Image.open(image_path)  # 이미지 파일 열기
-    start_time = time.time()
-    img = ImageOps.exif_transpose(img)
-    transform = T.Compose([
-            #T.Resize(520),
-            #T.CenterCrop(480),
-            T.ToTensor(),
-            T.Normalize(        
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225])
-            ])
-    trans_img = transform(img).permute(1, 2, 0)
-    img = transform(img).unsqueeze(0)
-    img = img.to('cuda')
-    out = model(img)['out']
-    print(img.shape)
-    print(out.shape)
-
-    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-    end_time = time.time()
-    print (om.shape)
-    print (np.unique(om))
-    rgb = decode_segmap(om)
-    print("prediction one image : ", round(end_time - start_time, 3))
-    plt.subplot(121), plt.axis('off'), plt.imshow(trans_img)
-    plt.subplot(122), plt.axis('off'), plt.imshow(rgb)
-    plt.show()
-'''
-
-
 import torch
 from PIL import Image, ImageOps
 import torchvision.transforms as T
@@ -84,7 +8,7 @@ import time
 # Define the helper function
 def decode_segmap(image, nc=3):
     label_colors = np.array([(0, 0, 0),  # 0=background
-                             # 1=ceiling, 2=floor, 3=wall, 4=blockage, 5=glassdoor
+                             #1=floor, 2=blockage
                              (255, 255, 255), (100, 100, 100),
                              ])
 
@@ -102,11 +26,11 @@ def decode_segmap(image, nc=3):
     return rgb
 
 # 모델 로드
-model = torch.load('D:\\pytorch_cuda\\segmentaion_data\\dataset8\\output\\model.pth', weights_only=False)
+model = torch.load('.\model.pth', weights_only=False)
 model.eval()
 
 # 비디오 경로 및 캡처 객체 정의
-video_path = 'D:\\pytorch_cuda\\segmentaion_data\\1080.mp4'
+video_path = 'INPUT YOUR VIDEO PATH'
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
@@ -119,10 +43,10 @@ frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # 출력 비디오 작성을 위한 코덱 및 VideoWriter 객체 정의
-output_path = 'D:\\pytorch_cuda\\segmentaion_data\\dataset7\\test_segmentation.mp4'
+output_path = 'INPUT YOUR RESULT VIDEO PATH'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out_video= cv2.VideoWriter(output_path, fourcc, fps, (1280, 480))
-output_mask_path = 'D:\pytorch_cuda\segmentaion_data\dataset7\mask_segmentation.mp4'
+output_mask_path = 'INPUT YOUR RESULT VIDEO PATH'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 mask_video= cv2.VideoWriter(output_mask_path, fourcc, fps, (640, 480))
 
