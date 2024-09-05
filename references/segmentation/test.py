06881d4e3107312a77ec5e87068f6fae843f989a
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-09-05 15:18:09 +0900

    Update test.py

--- references/segmentation/test.py
+++ references/segmentation/test.py
@@ -8,14 +8,7 @@ import cv2
 # Define the helper function
 def decode_segmap(image, nc=21): 
   label_colors = np.array([(0, 0, 0),  # 0=background
-               # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-               (128, 0, 0), (255, 255, 255), (128, 128, 0), (255, 0, 0), (128, 0, 128),
-               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+               # 1=floor, 2=blockage])
   r = np.zeros_like(image).astype(np.uint8)
   g = np.zeros_like(image).astype(np.uint8)
   b = np.zeros_like(image).astype(np.uint8) 
@@ -29,7 +22,7 @@ def decode_segmap(image, nc=21):
  
 
 # load pth model
-model = torch.load('model.pth')
+model = torch.load('./model.pth')
 # set model to inference mode
 model.eval()
 #print(model)
